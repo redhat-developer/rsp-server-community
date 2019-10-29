72b358a5d398566321a965c152e951a074fc4250
Rob Stryker <rob@oxbeef.net>
2019-10-29 15:34:58 -0400

    Remove wonka, prepare repo for tomcat
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- rsp/targetplatform/mavendeps/fetch_distribution.sh
+++ rsp/targetplatform/mavendeps/fetch_distribution.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget https://download.jboss.org/jbosstools/adapters/snapshots/rsp-server/org.jboss.tools.rsp.distribution-0.20.0-SNAPSHOT.zip
-unzip org.jboss.tools.rsp.distribution-0.20.0-SNAPSHOT.zip
+wget https://download.jboss.org/jbosstools/adapters/snapshots/rsp-server/org.jboss.tools.rsp.distribution-0.21.1-SNAPSHOT.zip
+unzip org.jboss.tools.rsp.distribution-0.21.1-SNAPSHOT.zip
