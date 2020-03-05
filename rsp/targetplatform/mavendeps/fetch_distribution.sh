86a2c6856a83b18b6ff5c00df03ab2409cd3e0f7
Rob Stryker <rob@oxbeef.net>
2020-03-05 14:13:19 -0500

    Fixes #6 - fetch_Distribution script no longer needed
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- rsp/targetplatform/mavendeps/fetch_distribution.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-wget https://download.jboss.org/jbosstools/adapters/snapshots/rsp-server/org.jboss.tools.rsp.distribution-0.22.2.Final.zip
-unzip org.jboss.tools.rsp.distribution-0.22.2.Final.zip
